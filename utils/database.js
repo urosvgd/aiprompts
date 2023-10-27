35952257b44016dc0aa861e4fe2d5d6a59031636
uros <uros@192.168.1.13>
2023-10-27 16:21:13 +0200

    added console log

--- utils/database.js
+++ utils/database.js
@@ -5,9 +5,11 @@ let isConnected = false; // track the connection
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
-  if(isConnected) {
+  if (isConnected) {
     console.log('MongoDB is already connected');
     return;
+  } else {
+    console.log("not connected to db")
   }
 
   try {
